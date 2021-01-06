3dc71a446197820ca5242b3df4da89741c32c543
AdrianoDOrazi <75425391+AdrianoDOrazi@users.noreply.github.com>
2021-01-06 23:42:53 +0100

    Add files via upload

---
+++ Stringhe.py
@@ -0,0 +1,29 @@
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> stringa=str(input("inserisci la parola"))
+inserisci la parola portapastelli
+>>> i=0
+>>> while i< len(stringa):
+	print(stringa[i])
+	i=i+1
+
+	
+ 
+p
+o
+r
+t
+a
+p
+a
+s
+t
+e
+l
+l
+i
+>>> print('i')
+i
+>>> print(i)
+14
+>>> 
